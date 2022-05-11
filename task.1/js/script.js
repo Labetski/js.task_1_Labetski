e89f13806a3cb5eeb7a4b4515d77f118892a5cc4
Labetski <101900095+Labetski@users.noreply.github.com>
2022-05-11 17:58:36 +0300

    Add files via upload

---
+++ task.1/js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+alert("Привет, это работа Лабецкого Александра");
+
+let planetName = "Земля";
+let yourAge = "32";
+let brokenCar = "Лада";
+let headerCollor = "Черный";
+let administratorOrNot = "Правда";
+
+console.log(planetName);
+console.log(yourAge);
+console.log(brokenCar);
+console.log(headerCollor);
+console.log(administratorOrNot);
+
+let firstName = "Александр";
+let lastName = "Лабецкий";
+
+console.log("Привет, меня зовут " + firstName, lastName + " мне " + yourAge + " лет");
+
+
+let nameTeacher = prompt("Кто промеряет мою работу?");
+console.log("Мою работу проверяет " + nameTeacher);
+
+let str = "String";
+console.log(typeof str);
+
+let num = 2000;
+console.log(typeof num);
+
+let testSkiped = false;
+console.log(typeof testSkiped);
+
+let month = undefined;
+console.log(typeof month);
+
+let data = null;
+console.log(typeof data);
+
+let newStr = "37";
+let news = Number(newStr);
+console.log(typeof news);
+let news_2 = String(newStr)
+console.log(typeof news_2);
+
+
+
+
+
+let newValue = true
+
+
+
+let oneName = "Ivan";
+let surname;
+
+oneName = surname;
+console.log(surname);
